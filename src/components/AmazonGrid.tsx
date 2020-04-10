954d74d2b7b6ff8aa6c861ac4c50f2a2b84a9fc8
Evangelos Pittas <evangelospittas@gmail.com>
2020-04-10 16:25:37 +0300

    Change products data source to Firebase.

--- src/components/AmazonGrid.tsx
+++ src/components/AmazonGrid.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 //@ts-ignore
 import ReactHtmlParser from 'react-html-parser';
+import * as firebase from "firebase/app";
 
 interface Props {
 
@@ -21,46 +22,43 @@ export default class AmazonGrid extends React.Component<Props, State> {
     }
 
     test1 = {
-        iFrame: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=GB&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=wanderlust0f-21&language=en_GB&marketplace=amazon&region=GB&placement=B07BWJKW15&asins=B07BWJKW15&linkId=84f4777d738119048c963f27d4a49384&show_border=true&link_opens_in_new_window=true&internal=1"></iframe>',
+        iframe: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=GB&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=wanderlust0f-21&language=en_GB&marketplace=amazon&region=GB&placement=B07BWJKW15&asins=B07BWJKW15&linkId=84f4777d738119048c963f27d4a49384&show_border=true&link_opens_in_new_window=true&internal=1"></iframe>',
         url: 'https://amzn.to/33205t8'
     }
 
     test2 = {
-        iFrame: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=GB&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=wanderlust0f-21&language=en_GB&marketplace=amazon&region=GB&placement=B07BWJKW15&asins=B07BWJKW15&linkId=84f4777d738119048c963f27d4a49384&show_border=true&link_opens_in_new_window=true"></iframe>',
+        iframe: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=GB&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=wanderlust0f-21&language=en_GB&marketplace=amazon&region=GB&placement=B07BWJKW15&asins=B07BWJKW15&linkId=84f4777d738119048c963f27d4a49384&show_border=true&link_opens_in_new_window=true"></iframe>',
         url: 'https://amzn.to/33205t8'
     }
 
     test3 = {
-        iFrame: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=GB&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=wanderlust0f-21&language=en_GB&marketplace=amazon&region=GB&placement=B07BWJKW15&asins=B07BWJKW15&linkId=84f4777d738119048c963f27d4a49384&show_border=true&link_opens_in_new_window=true"></iframe>',
+        iframe: '<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=GB&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=wanderlust0f-21&language=en_GB&marketplace=amazon&region=GB&placement=B07BWJKW15&asins=B07BWJKW15&linkId=84f4777d738119048c963f27d4a49384&show_border=true&link_opens_in_new_window=true"></iframe>',
         url: 'https://amzn.to/33205t8'
     }
 
     componentDidMount() {
-        // this.init();
-        this.setState({
-            products: [this.test1, this.test2, this.test3]
-        })
+        this.init();
+        // this.setState({
+        //     products: [this.test1, this.test2, this.test3]
+        // })
     }
 
     async init() {
-        // try {
-        //     const snap = await firebase.database().ref('webgains/hotOctopuss').once('value');
-        //     const productsJSON = await snap.val();
-        //     const products = this.parseProducts(productsJSON);
-        //     await this.setState({ products: products });
+        try {
+            const snap = await firebase.database().ref('amazon').once('value');
+            const productsJSON = await snap.val();
+            const products: any[] = [];
 
-        //     const element = document.querySelector(`.line-clamp`);
-        //     // console.log(`ele:  ${JSON.stringify(element)}`);
-        //     lineClamp(element, 3);
+            for (const id in productsJSON) {
+                const productJSON = productsJSON[id];
+                products.push(productJSON);
+            }
 
-        //     //@ts-ignore
-        //     const elements = ReactDOM.findDOMNode(this).getElementsByClassName('line-clamp');
-        //     for (const ele of elements) {
-        //         lineClamp(ele, 2);
-        //     }
-        // } catch (error) {
-        //     console.error(error);
-        // }
+            await this.setState({ products: products });
+
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     chunkArray(array: any[], chunkSize: number): (any[])[] {
@@ -111,7 +109,7 @@ export default class AmazonGrid extends React.Component<Props, State> {
                                                 >
                                                     {
                                                         //@ts-ignore
-                                                        ReactHtmlParser(product.iFrame)
+                                                        ReactHtmlParser(product.iframe)
                                                     }
                                                 </div>
                                             </div>
